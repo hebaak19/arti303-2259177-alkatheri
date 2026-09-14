f282e0c1d51256ccd3dfa4eb1439c6675e88616a
Hebah Alkatheri <hebaalkathiri9@gmail.com>
2026-09-14 16:35:52 +0300

    feat: completing all exercises on lab 3

---
+++ tests/test_text_utils.py
@@ -0,0 +1,19 @@
+"""Tests for src/text_utils.py"""
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "/src"))
+
+import text_utils
+
+
+def test_clean_name_whitespace():
+    # TODO
+    assert text_utils.clean_name("  sara   ali  ") == "Sara Ali"
+    assert text_utils.clean_name("lama") == "Lama"
+
+
+def test_clean_name_capitalisation():
+    # TODO
+    assert text_utils.clean_name("FAISAL ALHARBI") == "Faisal Alharbi"
+
